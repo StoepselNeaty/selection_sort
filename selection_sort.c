62887882156ed234b3687a6187cf0cb9b7253e50
rpwitt <richardphilipwitt@gmail.com>
2016-02-05 21:45:12 +0100

    small error

--- selection_sort.c
+++ selection_sort.c
@@ -12,7 +12,7 @@ void selectionSort(int arr[], int n) {
   int i, j, min_idx;
   
   for (i = 0; i < n-1; i++) {
-    min_idx = 1;
+    min_idx = i;
     
     for (j = i+1; j < n; j++) {  
       if (arr[j] < arr[min_idx]) {
