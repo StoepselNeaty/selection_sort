f4203451b305ad12ef59714f56eff94a6111830a
rpwitt <richardphilipwitt@gmail.com>
2016-02-05 21:46:48 +0100

    Update selection_sort.c

--- selection_sort.c
+++ selection_sort.c
@@ -14,6 +14,7 @@ void selectionSort(int arr[], int n) {
   for (i = 0; i < n-1; i++) {
     min_idx = i;
     
+    /* find the next smallest */
     for (j = i+1; j < n; j++) {  
       if (arr[j] < arr[min_idx]) {
         min_idx = j;
