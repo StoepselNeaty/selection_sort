a84849b246c0c1ee5613eed2fc3c93961e484304
rpwitt <richardphilipwitt@gmail.com>
2016-02-05 20:03:51 +0100

    Added braces for the one-line statement and braceless for-statement.

--- selection_sort.c
+++ selection_sort.c
@@ -2,25 +2,24 @@
 
 #include <stdio.h>
 
-void swap(int *xp, int *yp)
-{
+void swap(int *xp, int *yp) {
   int temp = *xp
   *xp = *yp
   *yp = temp
 }
 
-void selectionSort(int arr[], int n)
-{
+void selectionSort(int arr[], int n) {
   int i, j, min_idx;
   
-  for (i = 0; i < n-1; i++)
-  {
+  for (i = 0; i < n-1; i++) {
     min_idx = 1;
-    for (j = i+1; j < n; j++)
-      if (arr[j] < arr[min_idx])
+    
+    for (j = i+1; j < n; j++) {  
+      if (arr[j] < arr[min_idx]) {
         min_idx = j;
-        
+      } 
+      
       swap(&arr[min_idx], &arr[i]);
+    }
   }
 }
-  
