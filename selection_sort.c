626431a685d8d20e05484b55e0bd1090416fe85f
rpwitt <richardphilipwitt@gmail.com>
2016-02-05 16:38:01 +0100

    Initial input
    Not sure if the bracketing is ok.

---
+++ selection_sort.c
@@ -0,0 +1,26 @@
+/* selection sort in C */
+
+#include <stdio.h>
+
+void swap(int *xp, int *yp)
+{
+  int temp = *xp
+  *xp = *yp
+  *yp = temp
+}
+
+void selectionSort(int arr[], int n)
+{
+  int i, j, min_idx;
+  
+  for (i = 0; i < n-1; i++)
+  {
+    min_idx = 1;
+    for (j = i+1; j < n; j++)
+      if (arr[j] < arr[min_idx])
+        min_idx = j;
+        
+      swap(&arr[min_idx], &arr[i]);
+  }
+}
+  
