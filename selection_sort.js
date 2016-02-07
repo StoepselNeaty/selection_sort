ee5f0f42357eb83c19e0e4322b79b6d30c140a07
rpwitt <richardphilipwitt@gmail.com>
2016-02-07 15:51:22 +0100

    initial commit

---
+++ selection_sort.js
@@ -0,0 +1,19 @@
+function selectionSort (sortMe)
+{
+  var i, j, tmp, tmp2;
+  for (i=0; i < sortMe.length-1; i++)
+  {
+    tmp = i;
+    for (j=i+1; j < sortMe.length; j++)
+    {
+      if (sortMe[j] < sortMe[tmp]) {
+        tmp = j;
+      }
+    }
+    if (tmp!=i){
+      tmp2 = sortMe[tmp];
+      sortMe[tmp] = sortMe[i];
+      sortMe[i] = tmp2;
+    }
+  }
+}
